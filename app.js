af86ff71f9625734093bf8605917e2bb3442d30b
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-12 11:31:38 +0000

    Add ejs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, function () {
