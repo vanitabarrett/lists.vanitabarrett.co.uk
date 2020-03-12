2b48c9f0b5b72880eb282f56ab08319ef039aff2
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-12 21:13:41 +0000

    Add books and start search page

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/books', function (req, res) {
+  res.render('books');
+});
+
+app.get('/search', function (req, res) {
+  res.render('search');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
