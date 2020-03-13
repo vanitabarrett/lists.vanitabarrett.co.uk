b6f8bc40866328f4a663c4b9f12380fee442a6f9
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-13 12:48:40 +0000

    Set up database connection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+var config = require('./config/config.js');
+
+const connection = mysql.createConnection({
+  host: config.databaseHost,
+  user: config.databaseUsername,
+  password: config.databasePwd,
+  database: config.databaseName
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -14,7 +27,13 @@ app.get('/books', function (req, res) {
 });
 
 app.get('/search', function (req, res) {
-  res.render('search');
+  connection.query('SELECT * FROM list_books', (err,rows) => {
+    if (rows) {
+      res.render('search', { books: rows });
+    } else {
+      // TODO: handle error
+    }
+  });
 });
 
 app.listen(3000, function () {
