f286f69268b1a414b779ccb728e3f3a90f3eb813
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-15 21:19:58 +0000

    First draft of line graph

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
-var config = require('./config/config.js');
-
-const connection = mysql.createConnection({
-  host: config.databaseHost,
-  user: config.databaseUsername,
-  password: config.databasePwd,
-  database: config.databaseName
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
+var database = require('./database');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -22,29 +10,23 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/books', function (req, res) {
+app.get('/books', async function (req, res) {
   var doughnutDataQuery = "SELECT COUNT(*) AS count FROM list_books WHERE Status=4";
   var nonListBookCountQuery = "SELECT COUNT(*) AS count FROM nonlist_books WHERE Status=4";
+  var completedListBooksQuery = "SELECT completed_list_books.Year, GROUP_CONCAT(list_books.Title) AS \'Books\', COUNT(completed_list_books.Book_Id) AS \'Count\' FROM completed_list_books left join list_books on(completed_list_books.Book_Id=list_books.Id) GROUP BY completed_list_books.Year";
 
-  connection.query(doughnutDataQuery, (err,doughnutDataRows) => {
-    if (doughnutDataRows) {
-      connection.query(nonListBookCountQuery, (err,nonListBooksCompletedRows) => {
-        if (nonListBooksCompletedRows) {
-          res.render('books', {
-            doughnutData: doughnutDataRows[0].count,
-            nonListBooksCompleted: nonListBooksCompletedRows[0].count
-         });
-        } else {
-          // TODO: handle error
-        }
-      });
-    } else {
-      // TODO: handle error
-    }
-  });
+  const doughnutData = await database(doughnutDataQuery)
+  const nonListBooksCompletedData = await database(nonListBookCountQuery)
+  const completedListBooksData = await database(completedListBooksQuery)
+
+  res.render('books', {
+    doughnutData: doughnutData[0].count,
+    nonListBooksCompletedData: nonListBooksCompletedData[0].count,
+    completedListBooksData: JSON.stringify(completedListBooksData)
+ });
 });
 
-app.get('/search', function (req, res) {
+app.get('/search', async function (req, res) {
   var type = req.query.type
 
   if (type === "list") {
@@ -55,13 +37,8 @@ app.get('/search', function (req, res) {
     var query = "SELECT * FROM nonlist_books WHERE Status=2 ORDER BY Id DESC"
   }
 
-  connection.query(query, (err,rows) => {
-    if (rows) {
-      res.render('search', { books: rows });
-    } else {
-      // TODO: handle error
-    }
-  });
+  const books = await database(query)
+  res.render('search', { books });
 });
 
 app.listen(3000, function () {
---
+++ database.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+var config = require('./config/config.js');
+
+function queryDatabase(queryString, params) {
+  const connection = mysql.createConnection({
+    host: config.databaseHost,
+    user: config.databaseUsername,
+    password: config.databasePwd,
+    database: config.databaseName
+  });
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
+
+  return new Promise((resolve, reject) => {
+    connection.query(queryString, params, (error, results) => {
+      if (error) {
+        console.log(error); // eslint-disable-line no-console
+        reject(error);
+      }
+      resolve(results);
+    });
+
+    connection.end();
+  });
+}
+
+module.exports = queryDatabase
