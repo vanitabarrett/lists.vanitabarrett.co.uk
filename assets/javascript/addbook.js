5dbe120ca4dfac192f3e37762b8c184eddcb8c16
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-18 20:23:35 +0000

    Add modal template

--- assets/javascript/addbook.js
+++ assets/javascript/addbook.js
@@ -1,6 +1,9 @@
 var addBookButton = document.querySelector(".js-add-book");
+var modalBackground = document.querySelector(".js-modal-bg");
 var modal = document.querySelector(".js-modal");
 
 addBookButton.addEventListener("click", function() {
-  modal.classList += " books__modal--show"
+  modalBackground.classList += " js-modal-bg--show"
+  modal.classList += " js-modal--show"
+
 });
