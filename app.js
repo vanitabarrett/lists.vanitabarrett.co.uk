bdbf1f05dd539134db3b47bd2fc14208b081eb2f
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2022-03-26 20:16:23 +0000

    Get UNESCO sites from XML URL
    And add functionality to update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var database = require('./database');
+const express = require('express');
+const app = express();
+const database = require('./database');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -12,10 +12,10 @@ app.get('/', function (req, res) {
 
 app.get('/completion-report', async function (req, res) {
   var completedListBooksQuery = "SELECT completed_list_books.Year, list_books.Title FROM completed_list_books INNER JOIN list_books ON completed_list_books.Book_Id=list_books.Id ORDER BY completed_list_books.Year DESC"
-  var completedListBooks = await database(completedListBooksQuery)
+  var completedListBooks = await database.queryDatabase(completedListBooksQuery)
 
   var completedNonListBooksQuery = "SELECT completed_nonlist_books.Year, nonlist_books.Title FROM completed_nonlist_books INNER JOIN nonlist_books ON completed_nonlist_books.Book_Id=nonlist_books.Id ORDER BY completed_nonlist_books.Year DESC"
-  var completedNonListBooks = await database(completedNonListBooksQuery)
+  var completedNonListBooks = await database.queryDatabase(completedNonListBooksQuery)
 
   res.render('completion_report', { completedListBooks, completedNonListBooks });
 });
@@ -26,10 +26,10 @@ app.get('/books', async function (req, res) {
   var completedListBooksQuery = "SELECT completed_list_books.Year, COUNT(completed_list_books.Book_Id) AS \'Count\' FROM completed_list_books left join list_books on(completed_list_books.Book_Id=list_books.Id) GROUP BY completed_list_books.Year";
   var completedNonListBooksQuery = "SELECT completed_nonlist_books.Year, COUNT(completed_nonlist_books.Book_Id) AS \'Count\' FROM completed_nonlist_books left join nonlist_books on(completed_nonlist_books.Book_Id=nonlist_books.Id) GROUP BY completed_nonlist_books.Year";
 
-  const doughnutData = await database(doughnutDataQuery)
-  const nonListBooksCompletedData = await database(nonListBookCountQuery)
-  const completedListBooksData = await database(completedListBooksQuery)
-  const completedNonListBooksData = await database(completedNonListBooksQuery)
+  const doughnutData = await database.queryDatabase(doughnutDataQuery)
+  const nonListBooksCompletedData = await database.queryDatabase(nonListBookCountQuery)
+  const completedListBooksData = await database.queryDatabase(completedListBooksQuery)
+  const completedNonListBooksData = await database.queryDatabase(completedNonListBooksQuery)
 
   res.render('books', {
     doughnutData: doughnutData[0].count,
@@ -38,6 +38,76 @@ app.get('/books', async function (req, res) {
     completedNonListBooksData: JSON.stringify(completedNonListBooksData)
  });
 });
+
+app.post('/update', async function (req, res) {
+  // Fetch latest data from Unesco
+  var allUnescoSites = await database.fetchDataFromUnesco()
+  var unescoSitesArr = allUnescoSites.query.row.map((site) => {
+    return {
+      id: Number(site.id_number),
+      name: String(site.site),
+      country: String(site.states)
+    }
+  })
+
+  var insertNewUnescoSitesQuery = "INSERT INTO list__travel (`id`, `name`, `country`, `status`) VALUES"
+
+  unescoSitesArr.forEach(async (site, index) => {
+    if (index > 0) {
+      insertNewUnescoSitesQuery += ","
+    }
+
+    insertNewUnescoSitesQuery += "(" + site.id + ",\"" + site.name.replace(/"/g, '\\"') + "\",\"" + site.country.replace(/"/g, '\\"') + "\"," + 1 + ")"
+  })
+
+  await database.queryDatabase(insertNewUnescoSitesQuery)
+
+  res.redirect('/travel');
+})
+
+app.get('/update', async function (req, res) {
+  // Fetch latest data from Unesco
+  var allUnescoSites = await database.fetchDataFromUnesco()
+
+  var unescoIds = allUnescoSites.query.row.map((site) => {
+    return Number(site.id_number)
+  }).flat()
+
+  var unescoSitesArr = allUnescoSites.query.row.map((site) => {
+    return {
+      id: Number(site.id_number),
+      name: String(site.site),
+      country: String(site.states)
+    }
+  })
+
+  // Fetch all sites from database
+  var allSitesQuery = "SELECT id FROM list__travel"
+  const allSites = await database.queryDatabase(allSitesQuery)
+
+  var allSiteIds = allSites.map((site) => {
+    return site.id
+  })
+
+  // Identify any unesco sites not present in database (lookup via id)
+  var missingSiteIds = unescoIds.diff(allSiteIds)
+
+  if (missingSiteIds.length > 0) {
+    newSitesToAdd = []
+
+    missingSiteIds.forEach(missingSiteId => {
+      newSitesToAdd.push(unescoSitesArr.find(arr => arr.id === missingSiteId))
+    })
+
+    res.render('update', {
+      newSites: newSitesToAdd
+    });
+  } else {
+    res.render('update', {
+      error: 'There are no new sites to add'
+    });
+  }
+})
 
 app.get('/travel', async function (req, res) {
   var totalListedSitesQuery = "SELECT COUNT(*) AS count FROM list__travel"
@@ -48,11 +118,11 @@ app.get('/travel', async function (req, res) {
 
   var visitedCountriesQuery = "SELECT id, continent FROM nonlist_countries WHERE Status=1"
 
-  const totalListedSites = await database(totalListedSitesQuery)
-  const totalCompletedListedSites = await database(totalCompletedListedSitesQuery)
-  const totalCountries = await database(totalCountriesQuery)
-  const totalCompletedCountries = await database(totalCompletedCountriesQuery)
-  const visitedCountries = await database(visitedCountriesQuery)
+  const totalListedSites = await database.queryDatabase(totalListedSitesQuery)
+  const totalCompletedListedSites = await database.queryDatabase(totalCompletedListedSitesQuery)
+  const totalCountries = await database.queryDatabase(totalCountriesQuery)
+  const totalCompletedCountries = await database.queryDatabase(totalCompletedCountriesQuery)
+  const visitedCountries = await database.queryDatabase(visitedCountriesQuery)
 
   res.render('travel', {
     totalListedSites: totalListedSites[0].count,
@@ -76,7 +146,7 @@ app.get('/search', async function (req, res) {
       var query = "SELECT * FROM nonlist_books WHERE Status=2 ORDER BY Id DESC"
     }
 
-    const books = await database(query)
+    const books = await database.queryDatabase(query)
     res.render('search', { books, type });
   }
   else if (list === "travel") {
@@ -86,7 +156,7 @@ app.get('/search', async function (req, res) {
       var query = "SELECT * FROM nonlist_countries ORDER BY Status ASC"
     }
 
-    const sites = await database(query)
+    const sites = await database.queryDatabase(query)
     res.render('search', { sites, type });
   }
 });
@@ -123,9 +193,9 @@ app.post('/edit-book', async function (req, res) {
     var secondaryQuery = "DELETE FROM " + completed_database_name + " WHERE Book_Id=" + id
   }
 
-  await database(query)
+  await database.queryDatabase(query)
   if (secondaryQuery) {
-    await database(secondaryQuery)
+    await database.queryDatabase(secondaryQuery)
   }
 
   res.redirect('/search?type=' + type + '&list=books');
@@ -147,7 +217,7 @@ app.post('/edit-travel', async function (req, res) {
 
   var query = "UPDATE " + database_name + " SET Status='" + status + "' WHERE Id='" + id + "'"
 
-  await database(query)
+  await database.queryDatabase(query)
 
   res.redirect('/search?type=' + type + '&list=travel');
 });
@@ -165,16 +235,16 @@ app.post('/add-book', async function (req, res) {
     var query = "INSERT INTO nonlist_books (`Id`, `Title`, `Author_FirstName`, `Author_Surname`, `Status`) VALUES (" + null + ",'" + title + "','"  + firstname + "','" + surname + "'," + status + ")"
   }
 
-  await database(query)
+  await database.queryDatabase(query)
 
   if (year) {
     // We now need to add it to the completed table
     var getBookId = "SELECT Id FROM nonlist_books WHERE title='" + title + "'"
-    var rawId =  await database(getBookId)
+    var rawId =  await database.queryDatabase(getBookId)
     id = rawId[0].Id
 
     var secondaryQuery = "INSERT INTO completed_nonlist_books (`Book_Id`, `Year`) VALUES (" + id + "," + year + ") ON DUPLICATE KEY UPDATE `Year` = '"+ year + "'"
-    await database(secondaryQuery)
+    await database.queryDatabase(secondaryQuery)
   }
 
   res.redirect('/books');
--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql');
 var config = require('./config/config.js');
+const https = require('https');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
 
 function queryDatabase(queryString, params) {
   const connection = mysql.createConnection({
@@ -25,4 +28,34 @@ function queryDatabase(queryString, params) {
   });
 }
 
-module.exports = queryDatabase
+function fetchDataFromUnesco() {
+  // Fetch latest data from Unesco XML
+  return new Promise((resolve, reject) => {
+    https.get("https://whc.unesco.org/en/list/xml", function (xmlResult) {
+      let data = '';
+      xmlResult.on('data', function (stream) {
+        data += stream;
+      });
+      xmlResult.on('end', function () {
+        parser.parseString(data, function (error, result) {
+          if (error === null) {
+            resolve(result);
+          }
+          else {
+            reject(error);
+          }
+        });
+      });
+    });
+  })
+
+}
+
+Array.prototype.diff = function (comparisonArray) {
+  return this.filter(function (i) { return comparisonArray.indexOf(i) < 0; });
+};
+
+module.exports = {
+  queryDatabase,
+  fetchDataFromUnesco
+}
