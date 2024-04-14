e4895c1839d6f1552de29759c49956cd4123b4ac
Vanita Barrett-Smith <vanita.barrett-smith@which.co.uk>
2024-04-14 16:18:20 +0100

    Move from dist to public

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const database = require('./database');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use("/dist", express.static(__dirname + '/dist'));
+app.use("/public", express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index');
